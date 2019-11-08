fd1e50d9759894f3715c2c349a79c5c8ad80e43d
Pedro Costa <pedro.costa@he-arc.ch>
2019-11-08 16:36:38 +0100

    Init with exploration of the dataset

---
+++ settings.py
@@ -0,0 +1,10 @@
+import os
+
+root_path = os.path.dirname(os.path.abspath(__file__))  # path to this file
+
+files = dict(data_folder=os.path.join(root_path, 'data'))
+files.update(
+    dict(
+        armed_conflict=os.path.join(files['data_folder'], "armed_conflict")
+    )
+)
